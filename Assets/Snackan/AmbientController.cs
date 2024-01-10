571f9d9623bb39650df82924fac573440d6ac856
snackan <97677625+snackan@users.noreply.github.com>
2024-01-10 10:33:16 +0100

    Ambient del.1
    +Safe zones med ljud som fade in när man går in och fade ut när man går ut.
    +Support för flera safezones med olika ljud
    
    (Instruktioner finns i scenen)

---
+++ Assets/Snackan/AmbientController.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[]safeZones = {};
+    [SerializeField]
+    AudioSource audioSource;
+
+
+    [SerializeField]
+    AudioClip PlaceholderOne;
+
+    [SerializeField]
+    AudioClip PlaceholderTwo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+    void doSound(string name)
+    {
+        if (name == "SafeZone1")
+        {
+            audioSource.Stop();
+           audioSource.PlayOneShot(PlaceholderOne);
+        }
+        if (name == "SafeZone2")
+        {
+            audioSource.Stop();
+            audioSource.PlayOneShot(PlaceholderTwo);
+        }
+    }
+    IEnumerator TweenVolume(bool increase)
+    {
+        if (increase == true)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                audioSource.volume += 0.01f;
+                yield return new WaitForSeconds(0.01f);
+            }
+           
+          
+        }
+        if (increase == false)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                audioSource.volume -= 0.01f;
+                yield return new WaitForSeconds(0.01f);
+            }
+
+
+        }
+
+        yield return "finished";
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach (GameObject trigger in safeZones)
+        {
+            if (trigger == other.gameObject)
+            {
+               
+               
+                doSound(trigger.name);
+                audioSource.time = 0;
+                Debug.Log("Trigger Enter");
+                StartCoroutine(TweenVolume(true));
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        foreach (GameObject trigger in safeZones)
+        {
+            if (trigger == other.gameObject)
+            {
+                Debug.Log("Trigger Exit");
+                StartCoroutine(TweenVolume(false));
+            }
+        }
+    }
+}
