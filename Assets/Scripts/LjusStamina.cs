65cd19f30f0fafba4472135187b08fe715c3abd5
OscarL16 <oscar.lundberg.se@gmail.com>
2024-01-12 10:15:29 +0100

    stamina update

--- Assets/Scripts/LjusStamina.cs
+++ Assets/Scripts/LjusStamina.cs
@@ -6,15 +6,25 @@ public class LjusStamina : MonoBehaviour
 {
     public float Stamina = 100;
     public float MaxStamina = 100f;
+    public bool staminaD = false;
 
     public void Update()
     {
         if (Input.GetKey(KeyCode.LeftControl))
         {
-            Stamina = Stamina -= 1 * Time.deltaTime * 5;
+            staminaD = true;
+
             print("Working");
         }
+        if (staminaD == true)
+        {
+            Stamina = Stamina -= 1 * Time.deltaTime * 5;
+        }
 
+        if (Stamina < 1f)
+        {
+            staminaD = false;
+        }
 
     }
 }
