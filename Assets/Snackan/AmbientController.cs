78a8bf3bfdf9d6e1b160fb6fdf7496f7760001b3
snackan <97677625+snackan@users.noreply.github.com>
2024-01-12 10:22:12 +0100

    kommenterade kod

--- Assets/Snackan/AmbientController.cs
+++ Assets/Snackan/AmbientController.cs
@@ -1,26 +1,34 @@
+/*
+Snäckan
+2024-01-12 10:13
+*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AmbientController : MonoBehaviour
 {
+    [Header("Safe Zones")]
     [SerializeField]
     GameObject[]safeZones = {};
+
+
+    [Header("Config")]
     [SerializeField]
     AudioSource audioSource;
 
-
+    [Header("Audio")]
     [SerializeField]
     AudioClip PlaceholderOne;
-
     [SerializeField]
     AudioClip PlaceholderTwo;
 
-    public bool inSafeZone;
+
+    public bool inSafeZone; //Internal
     // Start is called before the first frame update
     void Start()
     {
-        inSafeZone = false;
+        inSafeZone = false; //Assume the player is not in a safe zone when they spawn
     }
 
     // Update is called once per frame
@@ -29,7 +37,7 @@ public class AmbientController : MonoBehaviour
         
     }
    
-    void doSound(string name)
+    void doSound(string name) //Play a sound based on the name of the safe zone
     {
         if (name == "SafeZone1")
         {
@@ -46,40 +54,40 @@ public class AmbientController : MonoBehaviour
     {
         if (increase == true)
         {
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 100; i++) //Repeat 100 times
             {
                 audioSource.volume += 0.01f;
-                yield return new WaitForSeconds(0.01f);
+                yield return new WaitForSeconds(0.01f); //Smooth linear increase in volume
             }
            
           
         }
         if (increase == false)
         {
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 100; i++) //Repeat 100 times
             {
                 audioSource.volume -= 0.01f;
-                yield return new WaitForSeconds(0.01f);
+                yield return new WaitForSeconds(0.01f); //Smooth linear decrease in volume
             }
 
 
         }
 
-        yield return "finished";
+        yield return "finished"; //End coroutine
 
     }
     private void OnTriggerEnter(Collider other)
     {
         foreach (GameObject trigger in safeZones)
         {
-            if (trigger == other.gameObject)
+            if (trigger == other.gameObject) //If player has collided with one of the safe zones
             {
 
-                inSafeZone = true;
+                inSafeZone = true; //Make sure the player is considered as inside a safe zone
                 doSound(trigger.name);
-                audioSource.time = 0;
+                audioSource.time = 0; //Reset the time position of the audio to make sure it plays from the beginning
                 Debug.Log("Trigger Enter");
-                StartCoroutine(TweenVolume(true));
+                StartCoroutine(TweenVolume(true)); //Tween volume to 1
             }
         }
     }
@@ -88,11 +96,11 @@ public class AmbientController : MonoBehaviour
     {
         foreach (GameObject trigger in safeZones)
         {
-            if (trigger == other.gameObject)
+            if (trigger == other.gameObject) //If player has exited one of the safe zones
             {
                 inSafeZone = false;
                 Debug.Log("Trigger Exit");
-                StartCoroutine(TweenVolume(false));
+                StartCoroutine(TweenVolume(false)); //Tween volume to 0
             }
         }
     }
--- Assets/Snackan/AmbientSounds.cs
+++ Assets/Snackan/AmbientSounds.cs
@@ -1,15 +1,25 @@
+/*
+Snäckan
+2024-01-12 10:20
+*/
 using System.Collections;
-
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AmbientSounds : MonoBehaviour
 {
+
     AmbientController ac;
+
+    [Header("Sounds")]
+
     [SerializeField]
     AudioClip[] Moving = { };
     [SerializeField]
     AudioClip[] Standstill = { };
+
+    [Header("Config")]
+
     [SerializeField]
     Rigidbody rb;
     [SerializeField]
@@ -20,57 +30,58 @@ public class AmbientSounds : MonoBehaviour
     void Start()
     {
         ac = FindObjectOfType<AmbientController>();
-        StartCoroutine(goofyuncle());
+        StartCoroutine(SoundLoop()); //Start the sound loop
     }
 
     IEnumerator TweenVolume(bool increase)
     {
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(3); //Wait until approximated end of sound
         if (increase == true)
         {
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 100; i++) //Repeat 100 times
             {
                 audioSource.volume += 0.01f;
-                yield return new WaitForSeconds(0.01f);
+                yield return new WaitForSeconds(0.01f); //Smooth linear increase of volume
             }
 
 
         }
         if (increase == false)
         {
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 100; i++) //Repeat 100 times
             {
                 audioSource.volume -= 0.01f;
-                yield return new WaitForSeconds(0.01f);
+                yield return new WaitForSeconds(0.01f);  //Smooth linear decrease of volume
             }
 
 
         }
 
-        yield return "finished";
+        yield return "finished"; //End coroutine
 
     }
-    IEnumerator goofyuncle()
+
+    IEnumerator SoundLoop()
     {
-    yield return new WaitForSeconds(20f); //initial wait to make the later parts more accurate
+    yield return new WaitForSeconds(20f); //Initial wait to make the later parts more accurate
     MarkerA:
         while (ac.inSafeZone == false)
         {
             if (rb.velocity.magnitude == 0)
             {
                 print("standstill");
-                Invoke("randomStandstillSound", Random.Range(0, 10));
+                Invoke("randomStandstillSound", Random.Range(0, 10)); //Start a random sound from the "Standstill" list within 0-10 seconds
                
             }
             else
             {
                 print("moving");
-                Invoke("randomWalkSound", Random.Range(10, 30));
-                yield return new WaitForSeconds(10f);
+                Invoke("randomWalkSound", Random.Range(10, 30)); //Start a random sound from the "Moving" list within 10-30 seconds
+                yield return new WaitForSeconds(10f); //Calibration; make sure sounds don't play too often
             }
         }
         yield return new WaitUntil(() => ac.inSafeZone == false);
-        goto MarkerA;
+        goto MarkerA; //Return to start of coroutine after inSafeZone becomes false
     }
    /* void PlaySound(AudioClip sound)
     {
@@ -83,23 +94,23 @@ public class AmbientSounds : MonoBehaviour
     }
     void randomWalkSound()
     {
-        audioSource.Stop();
+        audioSource.Stop(); //Stop possible overlapping from previous sounds/safe zone audio
         audioSource.volume = 1;
         Invoke("ResetVolume", 5);
-        AudioClip randomSound = Moving[Random.Range(0, Moving.Length)];
+        AudioClip randomSound = Moving[Random.Range(0, Moving.Length)]; //Get a random sound
         print(randomSound);
         audioSource.PlayOneShot(randomSound);
-        StartCoroutine(TweenVolume(false));
+        StartCoroutine(TweenVolume(false)); //Fade out the end of the sound
     }
     void randomStandstillSound()
     {
-        audioSource.Stop();
+        audioSource.Stop(); //Stop possible overlapping from previous sounds/safe zone audio
         audioSource.volume = 1;
         Invoke("ResetVolume", 5);
-        AudioClip randomSound = Standstill[Random.Range(0, Standstill.Length)];
+        AudioClip randomSound = Standstill[Random.Range(0, Standstill.Length)]; //Get a random sound
         print(randomSound);
         audioSource.PlayOneShot(randomSound);
-        StartCoroutine(TweenVolume(false));
+        StartCoroutine(TweenVolume(false)); //Fade out the end of the sound
     }
   
     // Update is called once per frame
