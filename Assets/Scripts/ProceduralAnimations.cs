0d88282bdb5165d2d08efeed870783a2c19c98dd
Mickman321 <97745994+Mickman321@users.noreply.github.com>
2024-01-15 11:23:10 +0100

    waedrsfgtdhfyg

---
+++ Assets/Scripts/ProceduralAnimations.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralAnimations : MonoBehaviour
+{
+    bool grounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
+    }
+}
